061a80781ba5cec1040577fc1afa4acc6a5fa730
chrud66 <chrud66@gmail.com>
2019-08-01 23:18:08 +0900

    add migrations

--- database/migrations/2019_08_01_140355_create_boards_table.php
+++ database/migrations/2019_08_01_140355_create_boards_table.php
@@ -14,8 +14,13 @@ class CreateBoardsTable extends Migration
     public function up()
     {
         Schema::create('boards', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->bigIncrements('id')->comment('고유번호');
+            $table->unsignedBigInteger('author_id')->comment('작성자 고유 번호');
+            $table->string('subject', 200)->comment('제목');
+            $table->text('content')->comment('내용');
+            $table->timestamp('created_at')->nullable()->comment('작성일시');
+            $table->timestamp('updated_at')->nullable()->comment('수정일시');
+            // $table->timestamps();
         });
     }
 
